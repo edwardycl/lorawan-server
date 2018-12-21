cb3c191daddbb73c1c2e983c076b09e1382dd5f3
YeonGyu Jeong <81887821@hanmail.net>
2018-12-21 16:54:21 +0900

    Send new TX cycle as (Temp % 10) + 10, removed unused handle_rxq overloads.

--- src/lorawan_application_air_light.erl
+++ src/lorawan_application_air_light.erl
@@ -30,24 +30,10 @@ handle_uplink(_Context, _RxQ, _LastMissed, _Frame) ->
 % the data structure is explained in
 % https://github.com/Lora-net/LoRaMac-node/blob/master/src/apps/LoRaMac/classA/LoRaMote/main.c#L207
 handle_rxq({_Network, _Profile, #node{devaddr=DevAddr}}, _Gateways, _WillReply,
-        #frame{port=2, data= <<LED, Press:16, Temp:16, _AltBar:16, Batt, _Lat:24, _Lon:24, _AltGps:16>>}, []) ->
-    % this is used in CN779, EU868, IN865, KR920
+        #frame{port=2, data= <<_LED, Press:16, Temp:16, _AltBar:16, Batt, _Lat:24, _Lon:24, _AltGps:16>>}, []) ->
     lager:debug("PUSH_DATA ~s ~w ~w ~w", [lorawan_utils:binary_to_hex(DevAddr), Press, Temp, Batt]),
-    % blink with the LED indicator
-    {send, #txdata{port=2, data= <<((LED+1) rem 2)>>}};
-handle_rxq({_Network, _Profile, #node{devaddr=DevAddr}}, _Gateways, _WillReply,
-        #frame{port=2, data= <<LED, Temp, Batt, _Lat:24, _Lon:24, _AltGps:16>>}, []) ->
-    % this is used in AS923, AU915, US915, US915_HYBRID
-    lager:debug("PUSH_DATA ~s ~w ~w", [lorawan_utils:binary_to_hex(DevAddr), Temp, Batt]),
-    % blink with the LED indicator
-    {send, #txdata{port=2, data= <<((LED+1) rem 2)>>}};
-% proprietary extension for self-tests
-handle_rxq({_Network, _Profile, #node{devaddr=DevAddr}}, _Gateways, _WillReply,
-        #frame{port=2, data= <<>>}, []) ->
-    lager:debug("PUSH_DATA ~s ignore empty frame", [lorawan_utils:binary_to_hex(DevAddr)]),
-    ok;
-handle_rxq(_Context, _Gateways, _WillReply, #frame{port=Port, data=Data}, []) ->
-    {error, {not_semtech_mote, {Port, Data}}}.
+    TxDutyCycle = ((Temp div 100) rem 10) + 10,
+    {send, #txdata{port=2, data = <<TxDutyCycle:16>>}}.
 
 handle_delivery({_Network, _Profile, _Node}, _Result, _Receipt) ->
     ok.
