2cca3d18b6ef7d9d68c248dd95a5a017446e3075
이영찬 <youngchan1115@gmail.com>
2018-12-22 18:48:28 +0900

    Move beacon sending to start_beacon

--- src/lorawan_gw_router.erl
+++ src/lorawan_gw_router.erl
@@ -139,21 +139,22 @@ handle_info(submit_stats, #state{request_cnt=RequestCnt, error_cnt=ErrorCnt}=Sta
     {noreply, State#state{request_cnt=0, error_cnt=0}};
 
 handle_info({beacon, BeaconInterval, {MAC, GWState}, Network, DevAddr, TxQ}, State) ->
-    lager:debug("Beacon sent to ~s", [lorawan_utils:binary_to_hex(DevAddr)]),
+    start_beacon({MAC, GWState}, Network, DevAddr, TxQ),
+    {noreply, State}.
+
+start_beacon({MAC, GWState}, Network, DevAddr, TxQ) ->
+    {ok, BeaconInterval} = application:get_env(lorawan_server, beacon_interval),
+    lager:debug("Start beacon to ~s with interval ~p", [lorawan_utils:binary_to_hex(DevAddr), BeaconInterval]),
     case mnesia:dirty_read(node, DevAddr) of
         [] ->
             timer:send_after(BeaconInterval, {beacon, {MAC, GWState}, Network, DevAddr, TxQ});
         [#node{has_downlink=_HasDownlink}=Node] ->
             {ok, PHYPayload} = lorawan_mac:encode_beacon(Network, Node),
             downlink({MAC, GWState}, Network, DevAddr, TxQ, PHYPayload),
+            lager:debug("Beacon sent to ~s", [lorawan_utils:binary_to_hex(DevAddr)]),
             timer:send_after(BeaconInterval, {beacon, {MAC, GWState}, Network, DevAddr, TxQ})
     end,
-    {noreply, State}.
-
-start_beacon({MAC, GWState}, Network, DevAddr, TxQ) ->
-    {ok, BeaconInterval} = application:get_env(lorawan_server, beacon_interval),
-    lager:debug("Start beacon to ~s with interval ~n", [lorawan_utils:binary_to_hex(DevAddr), BeaconInterval]),
-    timer:send_after(BeaconInterval, {beacon, BeaconInterval, {MAC, GWState}, Network, DevAddr, TxQ}).
+    ok.
 
 terminate(Reason, _State) ->
     % record graceful shutdown in the log
